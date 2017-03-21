b7ae05bc8656365e2ef752704e056ca8777e45c1
Mark Raynsford <code@io7m.com>
2017-03-21 17:44:37 +0000

    Update Maven path

--- check.sh
+++ check.sh
@@ -12,7 +12,7 @@ mvn clean package
 
 cd "${CURRENT_DIR}"
 java \
-  -jar ~/.m2//repository/com/github/siom79/japicmp/japicmp/0.9.5-SNAPSHOT/japicmp-0.9.5-SNAPSHOT-jar-with-dependencies.jar \
+  -jar $HOME/.m2/repository/com/github/siom79/japicmp/japicmp/0.9.5-SNAPSHOT/japicmp-0.9.5-SNAPSHOT-jar-with-dependencies.jar \
   --old old/target/japicmp-bug-20170321-1.0.0.jar \
   --new new/target/japicmp-bug-20170321-1.0.1.jar \
   --html-file report.html
